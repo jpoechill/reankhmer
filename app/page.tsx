2b03a0bb03623077b645265745f4a0482e198058
jpoechill <poamrong@rith.com>
2024-03-30 22:36:55 -0700

    add some responsive

--- app/page.tsx
+++ app/page.tsx
@@ -113,35 +113,38 @@ export default function Home() {
         <div className="text-center text-[14px]">
           Made with Support From ❤️
         </div>
-        <div className="flex flex-row gap-[60px] mt-10 justify-center text-sm">
-          <div className="text-center">
-            <Image src="/female-01.png" width={80} height={0} className="rounded-full bg-[#53c2a8] mx-auto mb-3" alt="Virak"></Image>
-            <span className="font-bold">Virak Pheach</span> (22) <br />
-            Phnom Penh 🇰🇭
-          </div>
-          <div className="text-center">
-            <Image src="/female-02.png" width={80} height={0} className="rounded-full bg-[#53c2a8] mx-auto mb-3" alt="Virak"></Image>
-            <span className="font-bold">Aun Heng</span> (22) <br />
-            Phnom Penh 🇰🇭
-          </div>
-          <div className="text-center">
-            <Image src="/male-01.png" width={80} height={0} className="rounded-full bg-[#53c2a8] mx-auto mb-3" alt="Virak"></Image>
-            <span className="font-bold">Virak Virath</span> (22) <br />
-            Siem Reap 🇰🇭
-          </div>
-          <div className="text-center">
-            <Image src="/male-02.png" width={80} height={0} className="rounded-full bg-[#53c2a8] mx-auto mb-3" alt="Virak"></Image>
-            <span className="font-bold">Sok Sok Chanet</span> (22) <br />
-            Battambang 🇰🇭
+        <div className="w-full flex justify-center">
+
+          <div className="grid grid-cols-2 w-full max-w-[500px] lg:max-w-[800px] max-auto px-10 lg:grid-cols-4 gap-10 gap-x-0 lg:gap-[60px] mt-10 justify-center text-sm">
+            <div className="text-center">
+              <Image src="/female-01.png" width={80} height={0} className="rounded-full bg-[#53c2a8] mx-auto mb-3" alt="Virak"></Image>
+              <span className="font-bold">Virak Pheach</span> (19) <br />
+              Phnom Penh 🇰🇭
+            </div>
+            <div className="text-center">
+              <Image src="/female-02.png" width={80} height={0} className="rounded-full bg-[#53c2a8] mx-auto mb-3" alt="Virak"></Image>
+              <span className="font-bold">Aun Heng</span> (22) <br />
+              Phnom Penh 🇰🇭
+            </div>
+            <div className="text-center">
+              <Image src="/male-01.png" width={80} height={0} className="rounded-full bg-[#53c2a8] mx-auto mb-3" alt="Virak"></Image>
+              <span className="font-bold">Virak Virath</span> (23) <br />
+              Siem Reap 🇰🇭
+            </div>
+            <div className="text-center">
+              <Image src="/male-02.png" width={80} height={0} className="rounded-full bg-[#53c2a8] mx-auto mb-3" alt="Virak"></Image>
+              <span className="font-bold">Sok Sok Chanet</span> (22) <br />
+              Battambang 🇰🇭
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="mt-[60px] w-full flex flex-col justify-center bg-[#111] text-white p-11 text-center">
+      <div className="mt-[60px] w-full flex flex-col justify-center bg-[url('/footer_bg.jpg')] bg-no-repeat bg-cover bg-center text-white p-11 text-center">
         <div className="text-[14px]">
           ABOUT THIS WEBSITE
         </div>
-        <div className="pt-5 w-full flex justify-center">
+        <div className="w-full flex justify-center">
           <div className="max-w-[800px] text-[14px]">
             At the heart of our endeavor lies a deep commitment to fostering cultural exchange and understanding. By providing translations of short stories from English into Khmer, we aim to celebrate the beauty of language and literature while simultaneously preserving and promoting the unique heritage of Cambodia. Our website is more than just a repository of translated stories; it is a testament to the power of language to unite, inspire, and enrich lives. Through our collective efforts, we hope to foster a deeper understanding and appreciation of both English and Khmer literature, strengthening the bonds that connect us as global citizens while celebrating the unique beauty of Cambodia&apos;s cultural heritage.
           </div>
