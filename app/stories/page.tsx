fb5156023c9bdefa78b784e56fc233320e0e6366
jpoechill <poamrong@rith.com>
2024-03-30 23:25:20 -0700

    refine 1st story

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -26,23 +26,43 @@ export default function Home() {
       </div>
 
       <div className="w-full flex items-center justify-center">
-        <div className="w-full px-10 max-w-[1100px] mb-10">
-          <div className={`${myFont.className}` + ' text-[34px] leading-[70px] font-medium tracking-wide w-full'}>
-            មាន​មាន់​ពីរ​ក្បាល​នៅ​ក្នុង​ចម្ការ​តែ​មួយ ហើយ​ពួក​គេ​បាន​ប្រយុទ្ធ​ដើម្បី​សម្រេច​ថា​អ្នក​ណា​គួរ​ធ្វើ​ជា​ម្ចាស់។ <br /><br />
-            ពេល​វាយ​គ្នា​ចប់ ជន​រង​គ្រោះ​ក៏​ទៅ​លាក់​ខ្លួន​នៅ​ជ្រុង​ងងឹត ។ <br /><br />
-            ខណៈ​ដែល​អ្នក​ឈ្នះ​បាន​ហោះ​ឡើង​ទៅ​លើ​ដំបូល​ក្រោល​ហើយ​ស្រែក​ឆោឡោ។<br /><br />
-            ប៉ុន្តែ​ឥន្ទ្រី​មួយ​ក្បាល​បាន​ឈ្លប​មើល​គាត់​ពី​ទី​ខ្ពស់​មក​លើ​មេឃ
-            ហើយ​ហោះ​ចុះ​ក្រោម​យក​គាត់​ចេញ។  <br /><br />
-            ភ្លាមៗនោះ Cock
-            ម្នាក់ទៀតបានចេញពីជ្រុងរបស់គាត់
-            ហើយគ្រប់គ្រង roost
-            ដោយគ្មានគូប្រជែង។
+        <div className="w-full px-10 max-w-[1100px] mb-0">
+          <div className='w-full'>
+            <div className={`${myFont.className}` + ' text-[34px] leading-[70px] font-medium tracking-wide w-full'}>
+              មាន​មាន់​ពីរ​ក្បាល​នៅ​ក្នុង​ចម្ការ​តែ​មួយ ហើយ​ពួក​គេ​បាន​ប្រយុទ្ធ​ដើម្បី​សម្រេច​ថា​អ្នក​ណា​គួរ​ធ្វើ​ជា​ម្ចាស់។
+            </div>
+            <div className="text-[18px] pl-5 mt-2 pb-12">
+              &#x3e; There were two Cocks in the same farmyard, and they fought to decide who should be master.
+            </div>
 
+            <div className={`${myFont.className}` + ' text-[34px] leading-[70px] font-medium tracking-wide w-full'}>
+              ពេល​វាយ​គ្នា​ចប់ ជន​រង​គ្រោះ​ក៏​ទៅ​លាក់​ខ្លួន​នៅ​ជ្រុង​ងងឹត ។ ខណៈ​ដែល​អ្នក​ឈ្នះ​បាន​ហោះ​ឡើង​ទៅ​លើ​ដំបូល​ក្រោល​ហើយ​ស្រែក​ឆោឡោ។
+            </div>
+            <div className="text-[18px] pl-5 mt-2 pb-12">
+              &#x3e; When the fight was over, the beaten one went and hid himself in a dark corner; while the victor flew up on to the roof of the stables and crowed lustily.
+            </div>
+
+            <div className={`${myFont.className}` + ' text-[34px] leading-[70px] font-medium tracking-wide w-full'}>
+              ប៉ុន្តែ​ឥន្ទ្រី​មួយ​ក្បាល​បាន​ឈ្លប​មើល​គាត់​ពី​ទី​ខ្ពស់​មក​លើ​មេឃ ហើយ​ហោះ​ចុះ​ក្រោម​យក​គាត់​ចេញ។
+            </div>
+            <div className="text-[18px] pl-5 mt-2 pb-12">
+              &#x3e; But an Eagle espied him from high up in the sky, and swooped down and carried him off.
+            </div>
+
+            <div className={`${myFont.className}` + ' text-[34px] leading-[70px] font-medium tracking-wide w-full'}>
+              ភ្លាមៗនោះមាន់មួយក្បាលទៀតបានចេញពីជ្រុងរបស់គាត់ ហើយគ្រប់គ្រងលើគ្រែដោយគ្មានគូប្រជែង។
+            </div>
+            <div className="text-[18px] pl-5 mt-2 pb-12">
+              &#x3e; Forthwith the other cock came out of his corner and ruled the roost without a rival.
+            </div>
+            <div className="w-full text-center mb-10">
+              •••
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col justify-center mt-20">
+      <div className="flex flex-col justify-center mt-12">
         <div className="text-center text-[14px]">
           Made with Support From ❤️
         </div>
