bdbe7b2ad730bf5414da8c01c68f3386ef50e518
jpoechill <poamrong@rith.com>
2024-03-31 17:53:39 -0700

    add thumbnails

--- app/page.tsx
+++ app/page.tsx
@@ -92,6 +92,19 @@ export default function Home() {
             <button className="py-1 mr-2 px-2 text-xs text-white rounded-md bg-[#429A85]">Aesop&apos;s Fable</button>
             <button className="py-1 mr-2 px-2 text-xs text-white rounded-md bg-[#429A85]">400 BCE</button>
           </div>
+          <div className="w-full">
+            <div className="w-full mb-1">
+              <div className="overflow-hidden rounded-2xl">
+                <a href="/stories">
+                  <Image src="/splash_thumbs/miceandweasels.png" className="hover:scale-[1.05] h-full w-full transition-all duration-500 cursor-pointer hover:grayscale-0" alt="INTERACTIVE SHORT STORIES IN KHMER" height={10000} width={800} />
+                </a>
+              </div>
+            </div>
+            <div className="mt-3 text-[16px] mb-2">The Mice and the Weasels</div>
+            <button className="py-1 mr-2 px-2 text-xs text-white rounded-md bg-[#429A85]">Easy</button>
+            <button className="py-1 mr-2 px-2 text-xs text-white rounded-md bg-[#429A85]">Aesop&apos;s Fable</button>
+            <button className="py-1 mr-2 px-2 text-xs text-white rounded-md bg-[#429A85]">400 BCE</button>
+          </div>
 
         </div>
       </div>
